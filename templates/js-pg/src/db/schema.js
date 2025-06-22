06e477359ba9cadc085833332e99673417924258
mrbubbles-src <manuel.fahrenholz@mrbubbles-src.dev>
2025-06-22 02:05:10 +0200

    refactor: update usersTable schema to use 'text' type for fields
    Replaced 'varchar' with 'text' for the 'id', 'username', 'email', and 'password' fields in the usersTable schema. This change simplifies the schema definition and ensures compatibility with varying input lengths.

--- templates/js-pg/src/db/schema.js
+++ templates/js-pg/src/db/schema.js
@@ -1,22 +1,39 @@
 import { sql } from 'drizzle-orm';
-import { boolean } from 'drizzle-orm/pg-core';
-import { pgEnum, pgTable, timestamp, varchar } from 'drizzle-orm/pg-core';
+import {
+  pgEnum,
+  pgTable,
+  timestamp,
+  boolean,
+  text,
+  check,
+} from 'drizzle-orm/pg-core';
 
 export const role = pgEnum('role', ['admin', 'user']);
 
-export const usersTable = pgTable('users', {
-  id: varchar({ length: 36 })
-    .primaryKey()
-    .default(sql`gen_random_uuid()`),
-  username: varchar({ length: 20 }).notNull().unique(),
-  email: varchar({ length: 255 }).notNull().unique(),
-  password: varchar({ length: 255 }).notNull(),
-  role: role().default('user'),
-  verified: boolean().default(false),
-  createdAt: timestamp({ precision: 3, mode: 'string' })
-    .default(sql`CURRENT_TIMESTAMP`)
-    .notNull(),
-  updatedAt: timestamp({ precision: 3, mode: 'string' })
-    .default(sql`CURRENT_TIMESTAMP`)
-    .notNull(),
-});
+export const usersTable = pgTable(
+  'users',
+  {
+    id: text()
+      .primaryKey()
+      .default(sql`gen_random_uuid()`),
+    username: text().notNull().unique(),
+    email: text().notNull().unique(),
+    password: text().notNull(),
+    role: role().default('user'),
+    verified: boolean().default(false),
+    createdAt: timestamp({ precision: 3, mode: 'string' })
+      .default(sql`CURRENT_TIMESTAMP`)
+      .notNull(),
+    updatedAt: timestamp({ precision: 3, mode: 'string' })
+      .default(sql`CURRENT_TIMESTAMP`)
+      .notNull(),
+  },
+  (table) => [
+    check('password_min_length', sql`char_length(${table.password}) >= 8`),
+    check('username_min_length', sql`char_length(${table.username}) >= 3`),
+    check(
+      'email_format',
+      sql`${table.email} ~* '^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$'`,
+    ),
+  ],
+);
